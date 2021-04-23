beddd04c0840a2511de89432def44932dea11a1a
John Sturgeon <john.sturgeon@me.com>
2021-04-22 20:39:24 -0700

    Moving away from configuration json file to .env (for Heroku)
    This will resolve issue #27

--- flask_site/app.py
+++ flask_site/app.py
@@ -1,12 +1,15 @@
 """ Flask application for Apex Legends API Tracker """
+import os
 from flask import Flask, render_template
 from apex_legends_api import ApexLegendsAPI, ALPlayer, ALPlatform, ALAction
 import flask_site.graphing as graphing
 from flask_site.apex_stats import PlayerData
-from flask_site.common_init import get_settings
+from dotenv import load_dotenv
+
+load_dotenv()
 app = Flask(__name__, template_folder='templates')
-settings = get_settings()
-api_key = settings['tracker_api_key'].get('value')
+api_key = os.getenv('APEX_LEGENDS_API_KEY')
+default_player = os.getenv('DEFAULT_PLAYER_NAME')
 apex_api: ApexLegendsAPI = ApexLegendsAPI(api_key=api_key)
 
 
@@ -17,7 +20,7 @@ def index():
 
 
 @app.route('/days/<player_name>')
-def days(player_name='GoshDarnedHero'):
+def days(player_name=default_player):
     """ List of player matches and some detail / day """
     platform = get_platform_for_player(player_name)
     player: ALPlayer = apex_api.get_player(name=player_name, platform=platform)
@@ -50,7 +53,7 @@ def get_platform_for_player(player: str) -> ALPlatform:
 
 def get_players() -> list[dict]:
     """ Helper method that returns a list of players being tracked """
-    first_event = apex_api.events('GoshDarnedHero', ALPlatform.PC, ALAction.INFO)[0]
+    first_event = apex_api.events(default_player, ALPlatform.PC, ALAction.INFO)[0]
     if not isinstance(first_event, dict):
         return list({})
     return first_event["data"]
