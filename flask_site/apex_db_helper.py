1a32b4425a325dfd4fa3536f9755fe45a675ea43
John Sturgeon <john.sturgeon@me.com>
2021-04-27 13:09:16 -0700

    fixed Mongo to use TLS

--- flask_site/apex_db_helper.py
+++ flask_site/apex_db_helper.py
@@ -18,7 +18,8 @@ class ApexDBHelper:
             username=os.getenv('MONGO_USERNAME'),
             password=os.getenv('MONGO_PASSWORD'),
             authSource=os.getenv('MONGO_DB'),
-            authMechanism='SCRAM-SHA-256'
+            authMechanism='SCRAM-SHA-256',
+            tls=True
         )
         self.database: Database = self.client.apex_legends
         self.basic_player_collection: Collection = self.database.basic_player
