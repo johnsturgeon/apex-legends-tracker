c2b3bef5c4ac241a9f002da6697715a6daf11613
John Sturgeon <john.sturgeon@me.com>
2021-05-03 10:42:24 -0700

    Fix deployment scripts to start DB / API scraper on deploy
    Resolves #61

--- bin/save_api_data_to_db.py
+++ bin/save_api_data_to_db.py
@@ -6,25 +6,30 @@ import player_manager
 from requests import exceptions
 
 
-def save_player_data():
+def save_player_data(loop_delay: int):
     """ Saves the player data record """
-    print(f"{arrow.now().format()}: Getting api data: ")
-    seconds = 50
     try:
         player_manager.save_player_data()
-        if player_manager.is_anyone_online():
-            seconds = 5
-        print(f"{arrow.now().format()}: Saved api data:  waiting {seconds} seconds")
-        time.sleep(seconds)
+        time.sleep(loop_delay)
     except exceptions.ConnectionError as error_message:
         print(error_message)
 
 
 if __name__ == "__main__":
+    MIN_LOOP_DELAY = 5
+    MAX_LOOP_DELAY = 50
+    LOOP_DELAY: int = MIN_LOOP_DELAY if player_manager.is_anyone_online() else MAX_LOOP_DELAY
+    PREVIOUS_LOOP_DELAY = LOOP_DELAY
+    print("hi")
     while True:
         try:
             requests.get("https://hc-ping.com/ac0f2dc2-5075-4a79-b644-16218967a293", timeout=10)
         except requests.RequestException as error:
             # Log ping failure here...
-            print("Ping failed: %s" % error)
-        save_player_data()
+            print(f"{arrow.now().format()}: Ping of healthchecks.io failed: {error}")
+        save_player_data(LOOP_DELAY)
+
+        LOOP_DELAY = MIN_LOOP_DELAY if player_manager.is_anyone_online() else MAX_LOOP_DELAY
+        if PREVIOUS_LOOP_DELAY != LOOP_DELAY:
+            print(f"{arrow.now().format()}: Changing Loop delay to {LOOP_DELAY}")
+        PREVIOUS_LOOP_DELAY = LOOP_DELAY
