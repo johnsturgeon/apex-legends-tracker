57bc6cd54455829726bf468a3869fcfdbb8e3068
John Sturgeon <john.sturgeon@me.com>
2021-05-15 15:38:41 -0700

    Fixed multiple issues connecting to the database by migrating to MongoDB Atlas (cloud) (#113)
    ## [0.5.5] - 2021-05-15
    ### Fixed
    - Fixed multiple issues connecting to the database by migrating to MongoDB Atlas (cloud)

--- flask_site/apex_db_helper.py
+++ flask_site/apex_db_helper.py
@@ -73,14 +73,10 @@ class LogHandler(logging.Handler):
 class ApexDBHelper:
     """ Class for retrieving / saving data to the Apex Mongo DB """
     def __init__(self):
-
-        self.client: MongoClient = MongoClient(
-            host=os.getenv('MONGO_HOST'),
-            username=os.getenv('MONGO_USERNAME'),
-            password=os.getenv('MONGO_PASSWORD'),
-            authSource=os.getenv('MONGO_DB'),
-            authMechanism='SCRAM-SHA-256'
-        )
+        uri = f"mongodb+srv://{os.getenv('MONGO_USERNAME')}:{os.getenv('MONGO_PASSWORD')}"
+        uri += f"@{os.getenv('MONGO_HOST')}/{os.getenv('MONGO_DB')}"
+        uri += "?retryWrites=true&w=majority"
+        self.client: MongoClient = MongoClient(uri)
         self.database: Database = self.client.apex_legends
         self.basic_player_collection: Collection = self.database.basic_player
         self.event_collection: Collection = self.database.event
