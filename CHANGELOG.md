e27ee3352cd0af47ae5ae1c4666de13cb427875b
John Sturgeon <john.sturgeon@me.com>
2021-06-12 11:20:58 -0700

    Emergency Fix (#162)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ Welcome to the 'Apex Legends Tracker' changelog
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.2.1] - 2021-06-12
+### Fixed
+- Emergency Fix to module import
+
 ## [1.2.0] - 2021-06-12
 ### Added
 - Added new 'Day Detail' page.  You can get there by clicking on your player profile, or using the main menu.
--- flask_site/models/__init__.py
+++ flask_site/models/__init__.py
@@ -2,12 +2,13 @@
 # pylint: disable=import-error
 from .basic_info import BasicInfo, RankedDivisionInfo, RankedSplit, RankTier, Division,\
     BasicInfoCollection
-from .event import GameEvent
+from .event import GameEvent, EventCollection
 from .player import Player, PlayerCollection
 from .tracker_info import TrackerInfo, TrackerInfoCollection
 __all__ = [
     'BasicInfo',
     'GameEvent',
+    'EventCollection',
     'RankedDivisionInfo',
     'RankedSplit',
     'RankTier',
