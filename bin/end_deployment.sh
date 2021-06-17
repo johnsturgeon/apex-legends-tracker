53f1a20a59ba3a0319b1e4e9052ccaea2ed73857
John Sturgeon <john.sturgeon@me.com>
2021-06-17 15:51:52 -0700

    Fixed deployment issue (#188)

--- bin/end_deployment.sh
+++ bin/end_deployment.sh
@@ -3,7 +3,6 @@
 # This script will put the site in maintenance mode, stop the DB scraper
 ###
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-cd $SCRIPT_PATH
 sudo monit stop save_api_data_to_db
 sudo supervisorctl restart apex-legends-tracker
-rm ../flask_site/maintenance
+rm $SCRIPT_PATH/../maintenance
