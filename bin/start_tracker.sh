ad14b88bb8c05808e28fcfb752f87f26073f16ba
John Sturgeon <john.sturgeon@me.com>
2021-06-17 15:39:12 -0700

    Fixed a couple deployment issues (#187)
    ## [1.2.13] - 2021-06-17
    ### Added
    - Added a `begin` and `end` deployment scripts (Resolves #185)
    - Fixed the 'start_tracker' script to not start if one is running already (Resolves #184)

--- bin/start_tracker.sh
+++ bin/start_tracker.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+PID=$(ps aux | grep 'save_api_data_to_db' | grep -v grep | awk '{print $2}')
+if [[ $PID > 0 ]] ; then
+  echo "Process is running already"
+  exit 1
+fi
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPT_PATH
 cd ../flask_site
