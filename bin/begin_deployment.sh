34bbc4ad59e9048b721b4fe8397c219a3b562bce
John Sturgeon <john.sturgeon@me.com>
2021-06-17 15:50:20 -0700

    Fixed deployment issue

--- bin/begin_deployment.sh
+++ bin/begin_deployment.sh
@@ -3,6 +3,5 @@
 # This script will put the site in maintenance mode, stop the DB scraper
 ###
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-cd $SCRIPT_PATH
-touch ../flask_site/maintenance
+touch $SCRIPT_PATH/../maintenance
 sudo monit stop save_api_data_to_db
