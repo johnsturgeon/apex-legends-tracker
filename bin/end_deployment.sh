559e9be3170344a17cf612d6b836f4435d60b316
John Sturgeon <john.sturgeon@me.com>
2021-06-17 15:56:41 -0700

    one more small deployment issue (#189)

--- bin/end_deployment.sh
+++ bin/end_deployment.sh
@@ -3,6 +3,6 @@
 # This script will put the site in maintenance mode, stop the DB scraper
 ###
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-sudo monit stop save_api_data_to_db
+sudo monit start save_api_data_to_db
 sudo supervisorctl restart apex-legends-tracker
 rm $SCRIPT_PATH/../maintenance
