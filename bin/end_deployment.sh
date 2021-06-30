6479b3fb835ae53ac84eaf340186103b76ab5630
John Sturgeon <john.sturgeon@me.com>
2021-06-30 16:33:16 -0700

    Fixed deployment so that it should just start the ingestion script without monit

--- bin/end_deployment.sh
+++ bin/end_deployment.sh
@@ -4,6 +4,6 @@
 ###
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 sudo monit start save_api_data_to_db
-sudo monit start respawn_ingestion
+$SCRIPT_PATH/start_ingestion.sh
 sudo supervisorctl restart apex-legends-tracker
 rm $SCRIPT_PATH/../maintenance
